d91fb2f5a9b85b7a19790475e89ae06960fa4022
Adina Feinstein <adina.d.feinstein@gmail.com>
2021-09-08 14:28:43 -0500

    updated plots script

--- scripts/plot.py
+++ scripts/plot.py
@@ -30,6 +30,15 @@ def ruwe_cmap():
     cm = make_cmap(clist1, name='ocean')
     return cm
 
+def parula_cmap(histplots):
+    parula = np.load('/Users/arcticfox/parula_colors.npy')
+    parulacmap = LinearSegmentedColormap.from_list('sequential',
+                                                   np.load('/Users/arcticfox/parula_data.npy'))
+    cinds = parula[np.linspace(0, 210, len(histplots), dtype=int)]
+    oppo = ['#a79ae6', '#84b3e3', '#6cdefa', '#aae8a0', '#f5cd54']
+    return parula, cinds, oppo
+    
+
 def plot_hr(stars, ruwe_cutoff=1.4, save=True, outputname='ruwe_hr.pdf'):
     
     cmd_bprp, cmd_mg, cmd_mass, cmd_teff = read_mamajek()
@@ -88,6 +97,69 @@ def plot_hr(stars, ruwe_cutoff=1.4, save=True, outputname='ruwe_hr.pdf'):
     ax2.set_rasterized(True)
     
     if save:
-        plt.savefig('ruwe_hr.pdf', dpi=250, rasterize=True, bbox_inches='tight')
+        plt.savefig(outputname, dpi=250, rasterize=True, bbox_inches='tight')
 
     return absmag
+
+
+def plot_slopes(histplots, lowlim, upplim, mcmc_fits, starbins,
+                save=True, outputname='amp_rates.pdf'):
+    
+    parula, cinds, oppo = parula_cmap(histplots)
+
+    fig, (ax1, ax2) = plt.subplots(figsize=(14,8), ncols=2,
+                                   gridspec_kw={'width_ratios':[12,0.5]})
+    
+    fig.set_facecolor('w')
+    cinds = parula[np.linspace(0, 210, len(histplots), dtype=int)]
+    
+    
+    for i in range(len(histplots)):
+        q = histplots[i][0] > 0
+        
+        ax1.plot(histplots[i][1][1:][q], histplots[i][0][q], 
+                 c=cinds[i], lw=2)
+        
+        ax1.scatter(histplots[i][1][1:][q], 
+                    y=histplots[i][0][q], 
+                    marker='o', c=cinds[i],
+                    s=100, edgecolor='k', 
+                    label='{} $\pm$ {}'.format(np.round(mcmc_fits[i][0],2),
+                                               np.round(mcmc_fits[i][1], 2)),
+                    zorder=100)
+        
+        ax1.fill_between(histplots[i][1][1:][q],
+                         y1=lowlim[i][q],
+                         y2=upplim[i][q],
+                         color=cinds[i], alpha=0.4, lw=0)
+        
+
+    ax1.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc='lower left',
+               ncol=2, mode="expand", borderaxespad=0.)
+    ax1.set_xscale('log')
+    ax1.set_yscale('log')
+    
+    ax1.set_xlabel('Flare Amplitude [%]')
+    ax1.set_ylabel('Flare Rate [Day$^{-1}$]')
+    
+    # Heinous colorbar hack #
+    ax2.set_xticks([])
+    ax2.set_xlim(0,1)
+
+    for i in range(len(cinds)):
+        ax2.plot(0.5, i+0.5, 's', ms=87,
+                 c=cinds[i])
+    ax2.yaxis.tick_right()
+    ax2.set_yticklabels(starbins)
+    ax2.set_ylim(0,5)
+    ax2.yaxis.set_label_position("right")
+    ax2.set_ylabel('Mass [M$_\odot$]')
+    
+    plt.subplots_adjust(wspace = 0.03)
+
+    ax1.set_rasterized(True)
+    ax2.set_rasterized(True)
+    
+    if save:
+        plt.savefig(outputname, rasterize=True, dpi=300,
+                    bbox_inches='tight', pad_inches=0)
